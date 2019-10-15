64099ad5052e8691203953e37a55d1417e9fea7e
pattanunNP <phattanun19@hotmail.com>
2019-10-15 13:47:26 +0700

    Function fix

--- functions/index.js
+++ functions/index.js
@@ -96,29 +96,3 @@ exports.recountData = functions
          return await counterRef.set(messagesData.numChildren());
      });
 
-exports.gendata = functions
-    .storage
-    .ref('/Labeled_Images/{userId}/{pushId}')
-    .onWrite(async (change) => {
-        const collectionRef = change.after.ref.parent;
-        const countRef = collectionRef
-            .parent
-            .child('Label_Counts');
-
-        let increment;
-        if (change.after.exists() && !change.before.exists()) {
-            increment = 1;
-        } else if (!change.after.exists() && change.before.exists()) {
-            increment = 0;
-        } else {
-            return null;
-        }
-
-        // Return the promise from countRef.transaction() so our function waits for this
-        // async event to complete before it exits.
-        await countRef.transaction((current) => {
-            return (current || 0) + increment;
-        });
-        console.log('Counter updated.');
-        return null;
-    });
