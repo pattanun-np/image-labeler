ce76477b44a138425db61be4dd006aac5e786431
pattanunNP <phattanun19@hotmail.com>
2019-10-19 12:41:00 +0700

    firebase config

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,5 +1,9 @@
 import firebase from 'firebase';
 import config from './config'
+import 'firebase/auth/'
+import 'firebase/database'
+import 'firebase/storage'
+import 'firebase/firestore'
 
 firebase.initializeApp(config);
 firebase.firestore().settings({timestampsInSnapshots:true})
