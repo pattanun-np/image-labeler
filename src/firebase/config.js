eabf7b2dca0132e1798ecb20d8221c9d607dbe75
pattanunNP <phattanun19@hotmail.com>
2019-11-29 13:26:01 +0700

    move db-fix

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    apiKey: "AIzaSyD2fftOayM9dduIhseo2WKwICBWPb_VupQ",
-    authDomain: "deeplearning-7f788.firebaseapp.com",
-    databaseURL: "https://deeplearning-7f788.firebaseio.com",
-    projectId: "deeplearning-7f788",
-    storageBucket: "deeplearning-7f788.appspot.com",
-    messagingSenderId: "746305078311"
-};
-export default config
